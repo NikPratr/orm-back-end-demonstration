de6ce7df1ddf146fb4513f91cb56e1c17b62dea9
Nick Prater <NikPratr@gmail.com>
2022-11-16 18:20:58 -0500

    Finished routes

--- models/index.js
+++ models/index.js
@@ -7,20 +7,22 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
+  onDelete: "CASCADE",
 });
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
-  onDelete: "CASCADE",
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
+  foreignKey: "product_id",
   through: {
     model: ProductTag,
   },
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
+  foreignKey: "tag_id",
   through: {
     model: ProductTag
   },
